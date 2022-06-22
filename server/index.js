e59687d04d9b7566afaff7b47dc013d2933ecf3e
babuece97 <babuece97@yahoo.com.com>
2022-06-22 18:58:36 +0200

    DB connected

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+ const { MongoClient, ServerApiVersion } = require('mongodb');
 require ('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -9,9 +10,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w1zgr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log(' car services DB activittedd')
+  // perform actions on the collection object
+  client.close();
+});
+
+
 // TO CHK THE connect, we make a API
 app.get('/',(req,res)=> {
-    res.send('IM running server CAR');
+    res.send('IM running server CARr');
 
 });
 app.listen(port,()=>{
