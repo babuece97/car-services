ced7aeb5df0d56e2d7ee3b27511dc9665e2a004b
babuece97 <babuece97@yahoo.com.com>
2022-07-25 21:51:37 +0200

    jwd decoded

--- server/index.js
+++ server/index.js
@@ -10,6 +10,23 @@ const app = express();
 /// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
+function verifyJWT(req,res, next) {
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send({message:'Sorry !!Unauthorized access'});
+    }
+    const token = authHeader.split(' ') [1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err, decoded)=>{
+        if(err){
+            return res.status (403).send({message:'SorrY FORbiDDEN acceSS'});
+        }
+        console.log('DecodeD', decoded);
+        req.decoded =decoded;
+    })
+    // console.log('inside Jwt verifies KORCHI',authHeader);
+    next();
+
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w1zgr.mongodb.net/?retryWrites=true&w=majority`;
@@ -64,13 +81,19 @@ async function run (){
         // ORDER COLLECTION api
 
             // to load the orders in order page
-         app.get('/order', async(req,res)=>{
-             const email=req.query.email;
+         app.get('/order', verifyJWT, async(req,res)=>{
+            const decodedEmail = req.decoded.email;
+            const email=req.query.email;
             // console.log(email);
+            if (email === decodedEmail) {
             const query={email:email};
             const cursor =orderCollection.find(query); // to load all
             const orders =await cursor.toArray();
             res.send(orders);
+            }
+            else{
+                res.status (403).send({message:'SOrrY!! ForBiden access'})
+            }
             
          })
 
