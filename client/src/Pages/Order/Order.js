7975c45388ba3dffbc05cc0463a1d9b57bd40acb
babuece97 <babuece97@yahoo.com.com>
2022-07-26 11:00:14 +0200

    http status

--- client/src/Pages/Order/Order.js
+++ client/src/Pages/Order/Order.js
@@ -2,20 +2,34 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import {useNavigate} from 'react-router-dom';
+import { signOut } from 'firebase/auth';
 
 const Order = () => {
+    const navigate = useNavigate();
     const [user]=useAuthState(auth);
     const [orders, setOrders]=useState([]);// default value in array
     useEffect(()=>{
         const getOrders = async()=>{
             const email =user.email;
             const url =`http://localhost:5000/order?email=${email}`;
-            const {data}=await axios.get(url, {
-                headers:{
-                    authorization:`Bearer ${localStorage.getItem('accessToken')}`
+            try {
+                const {data}=await axios.get(url, {
+                    headers:{
+                        authorization:`Bearer ${localStorage.getItem('accessToken')}`
+                    }
+                });
+                setOrders(data);
+
+            }
+            catch (error){
+                console.log(error.message);
+                if(error.response.status===401 || error.response.status ===403){
+                    signOut(auth);
+                    navigate('/login')
                 }
-            });
-            setOrders(data);
+
+            }
         }
         getOrders();
 
